6d053442182e09a431dbec11b4af61f901ce727a
Bakibillah <mb.billah8@gmail.com>
2017-03-27 19:41:33 +0600

    practise for navigation

---
+++ app/src/main/java/com/bakibillah/navapptvac/NavAppMainActivity.java
@@ -0,0 +1,38 @@
+package com.bakibillah.navapptvac;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class NavAppMainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_app_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
